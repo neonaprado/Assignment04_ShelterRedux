ac35f9bcc62470ebb4dc2bf8c7262b515ee4d63f
neonaprado <neonaprado@gmail.com>
2025-10-11 15:01:08 -0700

    Setup the main, settings, and draw classes and imported processing library

---
+++ CS162Assignment02ClassDesign/src/Sketch.java
@@ -0,0 +1,20 @@
+//Assignment number 2
+//Neona Prado
+//Project that creates a random movement when clicking on a dog object 
+import processing.core.PApplet;
+public class Sketch extends PApplet {
+    //runs settings once
+    public void settings() {
+        size(1040, 1400);
+    }
+
+    public void draw (){
+        background(200);
+    }
+    public static void main(String[] args) {
+        //tells main class to run the stuff in MySketch class
+        PApplet.main("Sketch");
+    }
+
+    
+}
