17aadfbc9430416d84db5e84fb93619b3d0e363f
neonaprado <neonaprado@gmail.com>
2025-10-29 13:54:50 -0700

    Reads data text files

--- Assignment03_ShelterRedux/src/MySketch.java
+++ Assignment03_ShelterRedux/src/MySketch.java
@@ -1,5 +1,6 @@
 import processing.core.PApplet;
 import processing.core.PFont;
+import processing.core.PImage;
 import processing.sound.SoundFile;
 
 /**
@@ -11,12 +12,34 @@ public class MySketch extends PApplet {
     private static final int FONT_SIZE = 12;
 
     // TODO: remove all these arrays and make the program work by reading in the contents of data.txt instead 
-    private static final String[] dogNames = { "Scooby-Doo", "Toto", "Spuds Mackenzie", "Snoopy" };
-    private static final String[] dogBreeds = { "Great Dane", "Cairn Terrier", "Bull Terrier", "Beagle" };
-    private static final int[] dogAges = { 10, 2, 7, 55 };
-    private static final String[] dogImages = { "assets/scoobydoo_small.png", "assets/toto_small.png", 
-            "assets/spuds_small.png", "assets/snoopy_small.png" };
+    //private static final String[] dogNames = { "Scooby-Doo", "Toto", "Spuds Mackenzie", "Snoopy" };
+    //private static final String[] dogBreeds = { "Great Dane", "Cairn Terrier", "Bull Terrier", "Beagle" };
+    ///private static final int[] dogAges = { 10, 2, 7, 55 };
+    ///private static final String[] dogImages = { "assets/scoobydoo_small.png", "assets/toto_small.png", 
+           // "assets/spuds_small.png", "assets/snoopy_small.png" };
+    private int loadAnimalFiles(){
+        String [] lines = loadStrings("assets/data.txt");
+        int count = 0;
+
+        for (String line : lines){
+            String[] f = line.split(",");
+            String name = f[0];
+            String breed = f[1];
+            int age = Integer.parseInt(f[2]);
+            PImage img = loadImage(f[3]);
+            SoundFile sound = new SoundFile(this, f[4]);
+            Dog dog = new Dog(name, breed, age, img);
+            dog.setSound(sound);
+            shelter.intake(dog);
+            count ++;
+
+            
+        }
+        return count;
+
+    }
     
+
     private Shelter shelter;
 
     public void settings() {
