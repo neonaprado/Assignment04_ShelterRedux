4486f28e3d5dbec659f422f7454f0a99d3ae0960
neonaprado <neonaprado@gmail.com>
2025-11-03 17:45:30 -0800

    creates project setup files and inital commit

---
+++ Exploration_Interfaces/Main.java
@@ -0,0 +1,26 @@
+/**
+ * Demonstrate the use of abstract classes
+ * @Author  YOUR NAME
+ *
+ * Do the following steps:
+ *
+ * Step 0: Add YOUR NAME above
+ *
+ * Step 1: Examine the code and notce the TODO's
+ *
+ * Step 2:  In Relatable.java, implement the isLargerThan method in the Relatable Interface
+ *
+ * Step 3:  In Main.java's main method. add the test cases described in the exploration and test.  It should print that rectangle 1 is the largest.
+ *
+ * Step 4: In Main.java's main method, create two more rectangles. This time, make the rectangle passed in as the argument the bigger one. Test.  
+ * 
+ * Step 5: In Main.java's main method, create two more rectangles. This time, make the rectangles the same size. Test. 
+ * 
+ * */
+
+ class Main {
+  
+    public static void main(String[] args) {
+      //TODO: Add the main method test code here
+    }
+  }
---
+++ Exploration_Interfaces/Rectangle.java
@@ -0,0 +1,33 @@
+public class Rectangle implements Relatable {
+    private int width;
+    private int height;
+    private int x,y;
+
+    public Rectangle(){
+      this(0,0,0,0);
+    }
+    public Rectangle(int x, int y, int width, int height) {
+        this.width = width;
+        this.height = height;
+        this.x=x;
+        this.y=y;
+    }
+   
+    public void move(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public int getArea() {
+        return width * height;
+    }
+    
+    // a method required to implement the Relatable interface
+    @Override
+    //TODO: Add the IsLargerThan method here
+  
+    @Override
+    public String toString(){
+      return "area: " + getArea();    
+    }
+}
