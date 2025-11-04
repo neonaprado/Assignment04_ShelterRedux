06a0032d57afc4614bb9f8c12669a96184d3c7a8
neonaprado <neonaprado@gmail.com>
2025-11-03 18:12:31 -0800

    implement reportCompare() to reduce the repition in testing

--- Exploration_Interfaces/Main.java
+++ Exploration_Interfaces/Main.java
@@ -19,6 +19,12 @@
  * */
 
  class Main {
+
+    private static void reportCompare(String label, Relatable a, Relatable b) {
+        int cmp = a.isLargerThan(b);
+        System.out.println(label + " -> cmp=" + cmp);
+        System.out.println(cmp > 0 ? "first is larger": (cmp < 0 ? "second is larger" : "same size"));
+      }
   
     public static void main(String[] args) {
         Rectangle r1 = new Rectangle(0, 0, 5, 4);
@@ -28,5 +34,7 @@
 
         System.out.println("case 1: " + r1 + " vs " + r2 + " -> cmp=" + cmp1);
         System.out.println(cmp1 > 0 ? "first is larger" : (cmp1 < 0 ? "second is larger" : "same size"));
+
+
   }
   }
